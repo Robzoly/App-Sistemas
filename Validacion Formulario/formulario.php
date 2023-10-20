42fd851c9c9b232c556921dc707f53f9402a652a
Robzoly <roberto23zeta@gmail.com>
2023-10-20 16:15:30 -0600

    Validación Formulario

--- Validacion Formulario/formulario.php
+++ Validacion Formulario/formulario.php
@@ -4,7 +4,7 @@ $host='localhost';
 $port='5432';
 $dbname='Base de datos 1';
 $user='postgres';
-$password=('319011340');
+$password='319011340';
 
 $conexion=pg_connect("host=$host dbname=$bd user=$user password=$pass");
 ?>
