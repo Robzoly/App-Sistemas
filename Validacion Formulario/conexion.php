a80839efd1c405970b9c6cff92ec0ad76e5f1595
Robzoly <roberto23zeta@gmail.com>
2023-10-20 17:21:17 -0600

    Validación Formulario

--- Validacion Formulario/conexion.php
+++ Validacion Formulario/conexion.php
@@ -1,23 +1,10 @@
 <?php
 
-require 'formulario.php';
+$host='localhost';
+$port='5432';
+$dbname='Base de datos 1';
+$user='postgres';
+$password='319011340';
 
-session_start();
-
-$usuario=$_POST['txtusuario'];
-$clave=$_POST['txtpassword'];
-
-$query=("SELECT*FROM usuarios WHERE 
-Usuario='$usuario' AND Contraseña='$clave");
-
-$consulta=pg_query($conexion,$query);
-$cantidad=pg_num_rows($consulta);
-
-if ($cantidad>0){
-
-    $_SESSION['nombre_usuario']=$usuario;
-    header('Location:ingreso.php');
-}
-else{
-    echo "Datos Incorrectos";
-}
+$conexion=pg_connect("host=$host dbname=$bd user=$user password=$pass");
+?>
